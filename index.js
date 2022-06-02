acd82aa02c009a7a8926a13ff938429799f1b51c
Shailaja327 <shailajar773@gmail.com>
2022-06-02 03:16:05 +0000

    commit message

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const { open } = require("sqlite");
-const sqlite3 = require("sqlite3");
 const path = require("path");
 
-const dbPath = path.join(__dirname, "goodreads.db");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
 const app = express();
+
+const dbPath = path.join(__dirname, "goodreads.db");
+
 let db = null;
+
 const initializeDBAndServer = async () => {
   try {
     db = await open({
@@ -13,18 +16,23 @@ const initializeDBAndServer = async () => {
       driver: sqlite3.Database,
     });
     app.listen(3000, () => {
-      console.log("Server running at http://localhost/Books/");
+      console.log("Server Running at http://localhost:3000/");
     });
   } catch (e) {
-    console.log(`DB Error:${e.message}`);
+    console.log(`DB Error: ${e.message}`);
     process.exit(1);
   }
 };
+
 initializeDBAndServer();
 app.get("/books/", async (request, response) => {
   const getBooksQuery = `
-   SELECT * FROM
-   book ORDERBY book_id;`;
-  const booksArray = await bd.all(getBooksQuery);
+    SELECT
+      *
+    FROM
+      book
+    ORDER BY
+      book_id;`;
+  const booksArray = await db.all(getBooksQuery);
   response.send(booksArray);
 });
